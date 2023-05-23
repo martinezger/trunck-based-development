07b28a81560e45cb04fb5e0eb1edb88871363572
Germán Martinez <germand_m@hotmail.com>
2023-05-23 16:14:30 -0300

    second commit

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/hello")
 def hello_world():
     return "<h1>HELLO WORLD!!</p>"
+
+
+
+@app.route("/goodbye")
+def goodby_world():
+    return "<h1>GOODBYE WORLD!!</p>"
